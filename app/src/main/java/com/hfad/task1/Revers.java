0b46d65f035ee2217f9a877d2b65c67ff5b7e1f9
Ion <ion.cumpata@gmail.com>
2023-02-07 20:17:39 +0200

    Lot of changes

---
+++ app/src/main/java/com/hfad/task1/Revers.java
@@ -0,0 +1,48 @@
+package com.hfad.task1;
+
+
+public class Revers {
+    public static void rev(char s[], int l, int r)
+    {
+        for (int i = l; i < r / 2; i++) {
+            char temp = s[i];
+            s[i] = s[r - 1 - i];
+            s[r - 1 - i] = temp;
+        }
+    }
+    public static void reverse(char s[])
+    {
+        // creating character array of size
+        // equal to length of string
+        char[] temp = new char[s.length];
+        int x = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z'
+                    || s[i] >= 'A' && s[i] <= 'Z') {
+                // storing elements in array
+                temp[x] = s[i];
+                x++;
+            }
+        }
+
+        // reversing the character array
+        rev(temp, 0, x);
+
+        x = 0;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] >= 'a' && s[i] <= 'z'
+                    || s[i] >= 'A' && s[i] <= 'Z') {
+                // updating the origional string
+                s[i] = temp[x];
+                x++;
+            }
+        }
+
+
+
+        String revStr = new String(s);
+        System.out.println(revStr);
+    }
+
+
+}
